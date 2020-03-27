220d8e0f3ea3009e7474da4958fb3da0951e9bf0
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-27 13:01:43 -0600

    Validaciones en submit en formulario reactivo

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -17,6 +17,16 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario() {
     this.forma = this.formBuilder.group({
       nombre  : ['', [Validators.required, Validators.minLength(5)]],
@@ -25,7 +35,12 @@ export class ReactiveComponent implements OnInit {
     });
   }
 
-  guardar(){
-    console.log(this.forma);
+  guardar() {
+    if (this.forma.invalid) {
+      Object.values(this.forma.controls).forEach( control => {
+        control.markAsTouched();
+      });
+      return;
+    }
   }
 }
