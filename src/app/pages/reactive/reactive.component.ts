995f195aa8c035c3245b12d5ca3b6a743e797f3d
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-27 12:32:15 -0600

    configuracion de formulario reactivo

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  
+  forma: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
   }
 
+  crearFormulario() {
+    this.forma = this.formBuilder.group({
+      nombre  : ['edwin'],
+      apellido: ['tello'],
+      correo  : ['edwintello@hotmail.com']
+    });
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
 }
